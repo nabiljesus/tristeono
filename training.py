045761204582d232161b5441ec2ec7cf7ab0f78a
Javier Lopez <javierloplom@gmail.com>
2017-03-28 14:49:25 -0400

    funciona adaboost

--- training.py
+++ training.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import cross_val_predict
+from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble        import AdaBoostClassifier
 
@@ -10,12 +11,16 @@ data.drop(data.columns[[0,2]],axis=1,inplace=True)
 # train, test = train_test_split(data, test_size = 0.2)
 
 
-clf = AdaBoostClassifier(n_estimators=10)
 
 target = data[[0]].replace('sadness','1.0').replace('happiness','0.0').astype('float64')
+target = pd.DataFrame.as_matrix(target).ravel()
 data.drop(data.columns[[0]],axis=1,inplace=True)
 
-predicted = cross_val_predict(clf, data, target.values, cv=10)
+# predicted = cross_val_predict(clf, data, target, cv=10)
+for i in [10,100,200,350]:
+    clf = AdaBoostClassifier(n_estimators=350)
+    predicted = cross_val_score(clf, data, target, cv=10)
+    print(predicted.mean())
 # import pdb; pdb.set_trace()
 
-print(predicted)
+# print(predicted)
